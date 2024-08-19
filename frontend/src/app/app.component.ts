38e704b329c6dc2ae43c59ab1c6fcb190f812a3b
Prajwal_Shrestha <prajwal.shrestha@bajratechnologies.com>
2024-08-19 18:13:51 +0545

    feat: add login service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/services/auth-services/auth.service';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.loginUser('Pk', '1245').pipe(
+      catchError((err) => {
+        return throwError(() => {
+          console.log(err);
+        })
+      })
+    ).subscribe((res) => {
+      console.log('Response:', res)
+    })
+  }
 }
