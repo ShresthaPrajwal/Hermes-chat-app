2739034da810ef8fbbf16aebf81b0629dda9a529
Prajwal_Shrestha <prajwal.shrestha@bajratechnologies.com>
2024-08-21 13:47:53 +0545

    chore: set up home components

---
+++ frontend/src/app/modules/home/chat-content/chat-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-content',
+  templateUrl: './chat-content.component.html',
+  styleUrl: './chat-content.component.scss'
+})
+export class ChatContentComponent {
+
+}
--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ChatContentComponent } from './chat-content/chat-content.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    ChatContentComponent
   ],
   imports: [
     CommonModule,
