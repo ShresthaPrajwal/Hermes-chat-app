fb29227f464e694f1cd08652f119a953fec57195
Prajwal_Shrestha <prajwal.shrestha@bajratechnologies.com>
2024-08-28 17:05:25 +0545

    feat: add logout button

--- frontend/src/app/modules/home/components/sidenav/sidenav.component.ts
+++ frontend/src/app/modules/home/components/sidenav/sidenav.component.ts
@@ -1,24 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../auth/services/user.service';
 import { TabService } from '../../services/tab/tab.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss',
+  styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent implements OnInit{
+export class SidenavComponent implements OnInit {
   public username: string = '';
   public profilePicture: string = '';
+  public displayLogoutDialog: boolean = false;
 
-  constructor(private userService: UserService, public tabService: TabService){}
+  constructor(
+    private userService: UserService,
+    public tabService: TabService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.username = this.userService.getUsername();
     this.profilePicture = this.userService.getProfilePicture();
   }
 
-  public selectTab(tab: string): void{
-    this.tabService.setSelectedTab(tab)
+  public selectTab(tab: string): void {
+    this.tabService.setSelectedTab(tab);
+  }
+
+  public confirmLogout(): void {
+    this.displayLogoutDialog = true;
+  }
+
+  public logout(): void {
+    this.userService.clearUserData();
+    this.displayLogoutDialog = false;
+    this.router.navigate(['/auth/login']);
   }
 }
